913bded455b3b983ac054a2d912a550edf31a611
hsokey-berk <hsokey@berkeley.edu>
2026-09-02 14:17:05 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-
-
-const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSwg6vUWuKAiBW4LqjVY428yO8bh9QfKYS6wNxU08Ga03a1KD17sw9YbebKcgupgdllcwP_QOsZQRJr/pub?output=csv";
+const CSV_FILENAME = "August2026.csv";
+ 
